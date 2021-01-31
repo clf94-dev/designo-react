4187ea43e201e292ad82f45952696fb39e5c018a
clf94-dev <clf94@hotmail.es>
2021-01-31 19:17:03 +0100

    added assets and favicon

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './styles/App.scss';
+
+function App(){
+    return(
+<div></div>
+    )
+}
+export default App;
