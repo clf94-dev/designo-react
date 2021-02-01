7cd5197dccd368177c72d8adcf53fbdee3d59aff
clf94-dev <clf94@hotmail.es>
2021-02-01 12:15:36 +0100

    added introHome component

--- src/components/IntroHome.jsx
+++ src/components/IntroHome.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Grid} from '@material-ui/core'
+
+import './styles/App.scss'
+
+import Hero from './assets/home/desktop/image-hero-phone.png';
+import Backg from './assets/home/desktop/bg-pattern-hero-home.svg';
 
 function IntroHome(){
     return(
-        <div>IntroHome component</div>
+        <div>
+            <Grid container direction="row" className="introHome-cont">
+                <img src={Backg} alt="circle background" className="bg-intro"/>
+                <Grid item xs={12} md={6} className="text-col">
+<h3>Award-winning custom designs and digital branding solutions</h3>
+<p>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
+          <button className="introHome-btn">Learn More</button>
+                </Grid>
+                <Grid item xs={12} md={6} className="img-col">
+                    <img src={Hero} alt="Smartphone"/>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 export default IntroHome;
