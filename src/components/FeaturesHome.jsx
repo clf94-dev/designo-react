e5dabbd26125ff784a6b91a63747ae5df94628c3
clf94-dev <clf94@hotmail.es>
2021-02-02 09:10:15 +0100

    added FeaturesHome component

--- src/components/FeaturesHome.jsx
+++ src/components/FeaturesHome.jsx
@@ -1,8 +1,28 @@
+import { Grid } from '@material-ui/core';
 import React from 'react'
+import {FeaturesHomeInfo} from './FeaturesHomeInfo.js'
+import PassionateD from './assets/home/desktop/illustration-passionate.svg'
+import ResourcefulD from './assets/home/desktop/illustration-resourceful.svg'
+import FriendlyD from './assets/home/desktop/illustration-friendly.svg'
 
 function FeaturesHome(){
+    const ImgD = [PassionateD, ResourcefulD, FriendlyD];
     return(
-        <div>FeaturesHome component</div>
+        <div>
+            <Grid container direction="row" className="features-cont">
+                {FeaturesHomeInfo.map(item => {
+                    return(
+                        <Grid item xs={12} sm={4} className="feat-col">
+                            <img src={ImgD[item.index]} alt={item.title}/>
+                            <h3>{item.title}</h3>
+                            <p>{item.text}</p>
+
+                        </Grid>
+                        
+                    );
+                })}
+            </Grid>
+        </div>
     )
 }
 export default FeaturesHome;
---
+++ src/components/FeaturesHomeInfo.js
@@ -0,0 +1,16 @@
+export const FeaturesHomeInfo = [{
+        index: 0,
+        title: 'PASSIONATE',
+        text: 'Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.'
+    },
+    {
+        index: 1,
+        title: 'RESOURCEFUL',
+        text: 'Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.'
+    },
+    {
+        index: 2,
+        title: 'FRIENDLY',
+        text: ' We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.'
+    }
+]
