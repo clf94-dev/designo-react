f69c59a3c84864b91120d0b0d2df5b650477e2f8
clf94-dev <clf94@hotmail.es>
2021-02-04 09:27:33 +0100

    cta mobile

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -5,7 +5,6 @@ function Cta(){
     return(
         <div>
             <Grid container direction="row" className="cta-cont">
-                <img src={Circles} alt="circles backg" className="circles"/>
                 <Grid item  xs={12} sm={6} className="text-col">
                     <h2>Let’s talk about your project</h2>
                     <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
