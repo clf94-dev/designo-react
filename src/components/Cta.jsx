9bcc78cb87f67af92d520fc4093d58c7269775ad
clf94-dev <clf94@hotmail.es>
2021-02-04 08:07:18 +0100

    added cta

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,8 +1,20 @@
+import { Grid } from '@material-ui/core';
 import React from 'react'
-
+import Circles from '../components/assets/shared/desktop/bg-pattern-call-to-action.svg'
 function Cta(){
     return(
-        <div>Cta component</div>
+        <div>
+            <Grid container direction="row" className="cta-cont">
+                <img src={Circles} alt="circles backg" className="circles"/>
+                <Grid item  xs={12} sm={6} className="text-col">
+                    <h2>Let’s talk about your project</h2>
+                    <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
+                </Grid>
+                <Grid item  xs={12} sm={6} className="btn-col">
+                    <button className="cta-btn">Get In Touch</button>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 export default Cta;
